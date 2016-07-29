c8ade49890b54298ec1d73e2eef7d33ac7fb06d2
Frank Kleine <frank.kleine@1und1.de>
2016-07-29 14:59:34 +0200

    use reflection method for invocation when we already have it

--- src/main/php/ClassProxyMethodHandler.php
+++ src/main/php/ClassProxyMethodHandler.php
@@ -85,7 +85,7 @@ trait ClassProxyMethodHandler
             // is_callable() returns true even for abstract methods
             $refMethod = new \ReflectionMethod(get_parent_class(), $method);
             if (!$refMethod->isAbstract()) {
-                return call_user_func_array(['parent', $method], $arguments);
+                return $refMethod->invokeArgs($this, $arguments);
             }
         }
 
